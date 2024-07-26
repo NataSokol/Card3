e468652acabacccf6bbed8a6ca8da2abe2a5d8c2
beAngelFace <be.angelface@gmail.com>
2024-07-26 12:00:19 +0300

    seedrs

--- server/db/seeders/20240726080523-CategoriesSeed.js
+++ server/db/seeders/20240726080523-CategoriesSeed.js
@@ -3,23 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+     await queryInterface.bulkInsert('Categories', [{
+      title: 'Попробуй отгадать загадки',
+      img: 'https://ixbt.online/live/images/original/10/48/54/2021/09/22/2d2f7b821d.jpg'
+
+
+      },
+      {
+      title: 'Насколько развита Ваша фотографическая память?',
+      img: 'https://v-kurse.ru/wp-content/uploads/2018/11/13a43ba3e4b91831b38e4c5d4712d2e4.jpg'
+      }
+    ], {});
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+     await queryInterface.bulkDelete('Categories', {});
+     
   }
 };
